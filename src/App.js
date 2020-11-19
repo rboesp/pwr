3a6d65062274fef75b536c5e1d648fd1a14c207a
Robert Boespflug <boespflr@oregonstate.edu>
2020-11-18 19:46:46 -0800

    Bootstrap set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Sidenav from "./components/Sidenav";
 import './App.css';
+import Container from "react-bootstrap/Container";
+import { Col, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      Hi!
-    </div>
-  );
+    <Container>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+      </Row>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+      </Row>
+    </Container>
+   
+  )
+    
 }
 
 export default App;
---
+++ src/components/Sidenav/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+function Sidenav() {
+  return (
+    <h1>Hi! there...</h1>
+  )
+    
+}
+
+export default Sidenav;
