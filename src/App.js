3e5830cee86b040d3ed2c0e44d7304c72ba86978
Robert Boespflug <boespflr@oregonstate.edu>
2021-03-22 08:54:28 -0700

    starting again 3/22/21

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from "react";
-import Sidenav from "./components/Sidenav";
 import './App.css';
 import Container from "react-bootstrap/Container";
 import { Col, Row } from "react-bootstrap";
 
 function App() {
-  return (
-    <Container>
-      <Row>
-        <Col>1</Col>
-        <Col>2</Col>
-      </Row>
-      <Row>
-        <Col>1</Col>
-        <Col>2</Col>
-      </Row>
-    </Container>
-   
-  )
-    
+  return ()
 }
 
 export default App;
