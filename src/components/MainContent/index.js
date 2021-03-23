00af012be9c9249946a31bb2d7f341723cc5c3c1
Robert Boespflug <boespflr@oregonstate.edu>
2021-03-23 12:30:21 -0700

    small gap gone in main content

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 export default class MainContent extends Component {
     render() {
         return (
-            <main className=' full-height main-content border border-warning'>
-                <figure className='border bg-dark quarter-height p-0 m-0'>
+            <main className=' full-height main-content'>
+                <figure className='bg-dark quarter-height p-0 m-0'>
                     <span className='bg-white'>Pic</span>
                     {/* <img src="https://via.placeholder.com/150" alt="Trulli"></img> */}
                 </figure>
-                <div className='border bg-light three-quarter-height'>
+                <div className='bg-light three-quarter-height'>
                     <div className='bio bg-info quarter-height'>
                         Bio
                     </div>
