39d1e70bed9104974788f32c25d8f0d8f04c6124
Robert Boespflug <boespflr@oregonstate.edu>
2021-03-23 15:28:38 -0700

    pic more responsive and looks better

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -5,8 +5,8 @@ export default class MainContent extends Component {
     render() {
         return (
             <main className=' full-height main-content'>
-                <figure className='p-0 m-0 border'>
-                    <img className='boobe' src={logo} alt=""></img>
+                <figure className='border border-info p-0 m-0 border'>
+                    <img className='full-height-width' src={logo} alt=""></img>
                 </figure>
                 <div className='bg-light three-quarter-height'>
                     <div className='bio quarter-height'>
