e20ca0a3a18e51eeec37fec1e9783609caa4007b
Robert Boespflug <boespflr@oregonstate.edu>
2021-03-24 15:01:19 -0700

    deleted space

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -27,7 +27,7 @@ export default class MainContent extends Component {
 
             default:
                 return <Homepage></Homepage>
-                break;
+            break;
         }
     }
 
