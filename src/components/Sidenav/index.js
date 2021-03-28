f2e128ea2e06e08d619602ca41dad977c41c5795
Robert Boespflug <boespflr@oregonstate.edu>
2021-03-28 13:07:30 -0700

    almost there with navbar

--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -29,27 +29,37 @@ export default class Sidenav extends Component {
     render() {
         return (
             <>
-                <div className='bg-dark sidenav-border sidenav-height d-flex flex-column justify-content-center'>
-                    <Navbar expand="lg" className="">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <div id='navbarSupportedContent' className='full-height-width'>
-                            <div className='full-height-width d-flex justify-content-center'>
-                                <div className='d-flex flex-column align-items-center full-width'>
-                                    <div className='full-width'>
-                                        {
-                                        this.state.nav_options.map(
-                                            option => {
-                                                console.log(option);
-                                                return <SideOption switchPage={this.props.switchPage} name={option.name} iconUrl={option.value}></SideOption>
-                                            })
-                                        }
-                                    </div>                                  
-                                </div>
+                <div className='bg-dark  sidenav-border sidenav-height'>
+                    <div className=' d-flex flex-column  full-height'>
+                        <Navbar expand="lg" className="d-flex flex-column full-height border border-danger">
+                            <div className='border border-warning p-2'>
+                                <h1 className='p-0 text-center white'>Robert Boespflug</h1>
                             </div>
-                        </div> 
-                    </Navbar.Collapse>
-                    </Navbar>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav" className=''>
+                                <div className=' d-flex flex-column border justify-content-between'>
+                                    <div id='navbarSupportedContent' className='full-height-width '>
+                                        <div className='  full-height-width d-flex justify-content-center'>
+                                            <div className='d-flex flex-column align-items-center full-width'>
+                                                <div className='full-width'>
+                                                    {
+                                                    this.state.nav_options.map(
+                                                        option => {
+                                                            console.log(option);
+                                                            return <SideOption switchPage={this.props.switchPage} name={option.name} iconUrl={option.value}></SideOption>
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div  id='navbarSupportedContent' className=' m-0 p-0'>
+                                        <h1 className='white text-center'>Hey there</h1>
+                                    </div>
+                                </div>
+                            </Navbar.Collapse>
+                        </Navbar>
+                    </div>
                 </div>
             </>
         )
