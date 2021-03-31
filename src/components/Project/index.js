86296d2f2e3daaee563eff4b3aa64004b1d20e48
Robert Boespflug <boespflr@oregonstate.edu>
2021-03-31 15:59:11 -0700

    projects now side-by-side

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Pill from '../Pill'
-import {Row, Col} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
 
 export default class Project extends Component {
 
@@ -22,40 +22,38 @@ export default class Project extends Component {
             pills
         } = details;
         return (
-            <div className='m-5'>
-                <Row class="">
-                    <Col xs={12} class="card-body border border-warning">
-                        <img className='card-img-top'
-                            style={
-                                {
-                                    height: '250px',
-                                    width: '250px'
+            <Col xs={6} >
+                <div class="card-body border border-warning">
+                            <img className='card-img-top'
+                                style={
+                                    {
+                                        height: '250px',
+                                        width: '250px'
+                                    }
                                 }
-                            }
-                        ></img>
-                        <h5 class="card-title">
-                            {name}
-                        </h5>
-                        <div className='d-flex'>
-                            {
-                                pills.map(
-                                    pill => <>
-                                        <div className='pillContainer m-1'>
-                                            <Pill pillData={pill}/>
-                                        </div>
-                                    </>
-                                )
-                            }
+                            ></img>
+                            <h5 class="card-title">
+                                {name}
+                            </h5>
+                            <div className='d-flex'>
+                                {
+                                    pills.map(
+                                        pill => <>
+                                            <div className='pillContainer m-1'>
+                                                <Pill pillData={pill}/>
+                                            </div>
+                                        </>
+                                    )
+                                }
+                            </div>
+                            <p class="card-text">
+                                {desc}
+                            </p>
+                            <a href={img} target="_blank" class="badge">
+                                {urlBlurb}
+                            </a>
                         </div>
-                        <p class="card-text">
-                            {desc}
-                        </p>
-                        <a href={img} target="_blank" class="badge">
-                            {urlBlurb}
-                        </a>
-                    </Col>
-                </Row>
-            </div>
+            </Col>
         )
     }
 }
