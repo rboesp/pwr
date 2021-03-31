c0c1d999902104accf051327a4f10a058ac497dc
Robert Boespflug <boespflr@oregonstate.edu>
2021-03-31 13:56:04 -0700

    have project cards setup

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Project from '../Project'
+
+export default class ProjectCard extends Component {
+
+    state = {
+        projects: [
+            {
+                name: 'Online Pictionary',
+                img: '',
+                desc: '',
+                url: ''
+            }
+        ]
+    }
+
+
+    render() {
+        return (
+            <div className='project-wrapper row  align-items-center'>
+                <h1 className='col-6 text-center'>
+                    {this.props.name}
+                </h1>
+                <img src={this.props.img} style={
+{                      height: '20%',
+                      width: '20%',
+                      borderRadius: '5%'}
+                }></img>
+            </div>
+            <div>
+                {
+                    this.state.projects.map(proj => <Project details={proj}></Project>)
+                }
+            </div>
+        )
+    }
+}
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
+import ProjectCard from '../ProjectCard'
 
 // border border-danger
 
 export default class Projects extends Component {
+
+    state = {
+        technologies : [
+            {
+                name: 'JavaScript',
+                img: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'
+            }
+        ]
+    }
+
     render() {
         return ( 
             <>
-                <div className='project-page-container border border-danger'>
+                <div className='project-page-container border border-danger m-5 bg-white'>
                     <div className='projects-title'>
-
+                        <h1>Projects</h1>
                     </div>
-                    <div className='projects-area'>
-
+                    <div className='projects-area border border-info m-5'>
+                        {
+                            this.state.technologies.map( tech => <ProjectCard name={tech.name} img={tech.img}/>)
+                        }
                     </div>
                 </div>
             </>
