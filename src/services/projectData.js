fa7cb89b72688c3b1d535bf46e68401b728fb698
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-03 07:34:51 -0700

    narrowed down three projects for now

--- src/services/projectData.js
+++ src/services/projectData.js
@@ -4,7 +4,7 @@ import stacks from './techStacks'
 
 const projects =  [
     {
-        name: 'Online Pictionary',
+        name: 'Online Whiteboard',
         img: '', //or gif
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         urlBlurb: 'Play online now!',
@@ -12,20 +12,20 @@ const projects =  [
         pills: getPillData(['React', 'Node.js', 'Express'])
     },
     {
-        name: 'Email Scheduling and Webhook Service',
-        img: '', //or gif
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        urlBlurb: 'Use it now!',
-        url: '',
-        pills: getPillData(['Node.js'])
-    },
-    {
         name: 'Country Comparison Tool',
         img: '', //or gif
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         urlBlurb: 'Use it now!',
         url: '',
-        pills: getPillData(['Chart.js', 'Node.js',])
+        pills: getPillData(['Chart.js', 'Node.js', 'Express'])
+    },
+    {
+        name: 'Network Pong',
+        img: '', //or gif
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        urlBlurb: 'Use it now!',
+        url: '',
+        pills: getPillData(['Node.js', 'Express'])
     },
 ]
 
