3a2b2bfd269d39b01541c90f5e663f92d818be3f
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-03 08:24:45 -0700

    more refactoring of sidenav index.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Sidenav from './components/Sidenav'
 
 class App extends React.Component {
   state = {
-    currentPage: 'Home'
+    currentPage: 'Resume'
   }
 
   switchPage = page => {
--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import  SideOption from "../SideOption/";
-import { Navbar } from 'react-bootstrap';
 import './style.css'
 import SideIcon from '../SideIcon';
 
@@ -8,12 +7,9 @@ export default class Sidenav extends Component {
     state = {
         icon_size : 35,
         nav_options : [
-            {
-            name: `Bio`            },
-            {
-            name: `Projects`            },
-            {
-            name: `Resume`            }
+            { name: `Bio` },
+            { name: `Projects` },
+            { name: `Resume` }
         ],
         icons: [
             {
@@ -29,41 +25,37 @@ export default class Sidenav extends Component {
 
     render() {
         return (
-            <>
-            {/* sidenav-border */}
-                <div className='bg-dark border border-danger sidenav-height '>
-                    <div className='border border-primary full-height d-flex flex-column justify-content-between'>
-                        {/* <Navbar expand="lg" className=" p-0 m-0 full-height "> */}
-                             <div className=' full-width p-2'>
-                                <h1 className='p-0 text-center white'>Robert Boespflug</h1>
-                                <h6 className='p-0 text-center white'>Software Developer </h6>
-                                <h6 className='p-0 text-center white'>Seattle, WA</h6>
-                            </div>
-                            <div id="basic-navbar-nav" className=' '>
-                                <div className=''>
-                                    <div className=''>
-                                        <div className=' '>
-                                            <div className=' '>
-                                                {
-                                                this.state.nav_options.map(
-                                                    option => {
-                                                        return <SideOption switchPage={this.props.switchPage} name={option.name} iconUrl={option.value}></SideOption>
-                                                    })
-                                                }
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className=' d-flex justify-content-around '>
-                                {
-                                    this.state.icons.map( icon => <SideIcon href={icon.href} src={icon.src} />)
-                                }
-                            </div>
-                        {/* </Navbar> */}
+            <div className='bg-dark sidenav-border sidenav-height '>
+                <div className=' full-height d-flex flex-column justify-content-between'>
+                        <div className=' full-width p-2'>
+                        <h1 className='p-0 text-center white'>Robert Boespflug</h1>
+                        <h6 className='p-0 text-center white'>Software Developer </h6>
+                        <h6 className='p-0 text-center white'>Seattle, WA</h6>
+                    </div>
+                    <div id="basic-navbar-nav">
+                        <div>
+                            {
+                                this.state.nav_options.map(
+                                    option => {
+                                        return ( 
+                                            <SideOption 
+                                                switchPage={this.props.switchPage} 
+                                                name={option.name} 
+                                                iconUrl={option.value}>
+                                            </SideOption>
+                                        )
+                                    }
+                                )
+                            }
+                        </div>
+                    </div>
+                    <div className=' d-flex justify-content-around '>
+                        {
+                            this.state.icons.map( icon => <SideIcon href={icon.href} src={icon.src} />)
+                        }
                     </div>
                 </div>
-            </>
+            </div>
         )
     }
 }
