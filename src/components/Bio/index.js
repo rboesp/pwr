e7d8aab8c262f7042bd6d7bb43899667500ed794
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-06 11:54:54 -0700

    changed bio a bit

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -3,17 +3,17 @@ import React from "react"
 export default function Bio(props) {
     return (
         <div className="row">
-            <h5 className="text col-12">
+            <h4 className="text col-12">
                 <span className="size">Welcome</span> to my personal site. I am a CS grad from Oregon State University now living in
                 Seattle, WA.
-            </h5>
-            <h5 className="text col">
+            </h4>
+            <h4 className="text col-12">
                 <span className="size">Currently</span> teaching Full Stack Development at University of Washington's Full Stack Coding
                 Bootcamp.
-            </h5>
-            <h5>
-                <span className="size col">Open</span>to software development positions in the Seattle area.
-            </h5>
+            </h4>
+            <h4 className="text col-12">
+                <span className="size mr-1">Open</span>to software development positions in the Seattle area.
+            </h4>
         </div>
     )
 }
