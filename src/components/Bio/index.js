ff9e5c79594356aeb409485059143ad6c545e65e
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-05 21:15:25 -0700

    side options now clickable all the way thorugh

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,24 +1,18 @@
-import React from 'react'
-import Resume from '../Resume'
+import React from "react"
 
 export default function Bio(props) {
     return (
         <div className="row">
             <h5 className="text col-12">
-                <span className='size'>Welcome</span> to my personal site. I am a 
-                CS grad from Oregon State University now living 
-                in Seattle, WA.
+                <span className="size">Welcome</span> to my personal site. I am a CS grad from Oregon State University now living in
+                Seattle, WA.
             </h5>
             <h5 className="text col">
-                <span className='size'>Currently</span> teaching Full Stack Development at 
-                University of Washington's Full Stack Coding 
+                <span className="size">Currently</span> teaching Full Stack Development at University of Washington's Full Stack Coding
                 Bootcamp.
             </h5>
             <h5>
-                <span className='size col'>
-                    Open
-                </span>to software development positions in
-                the Seattle area.
+                <span className="size col">Open</span>to software development positions in the Seattle area.
             </h5>
         </div>
     )
--- src/components/SideOption/index.js
+++ src/components/SideOption/index.js
@@ -1,25 +1,18 @@
-import React from 'react'
-import './style.css'
-import { Link  } from 'react-router-dom';
-import Resume from '../Resume';
+import React from "react"
+import "./style.css"
+import { Link } from "react-router-dom"
+import Pdf from "../../resume_pdf/Boespflug_Resume.pdf"
 
-export default function SideOption(
-    {
-        name,
-        url
-    }
-    ) {
-    return (
-        <div className='p-2 sideBtns d-flex justify-content-center'>
-            <div className=''>
-                <h5 className='p-1 option-names'>
-                {
-                    name === 'Resume' ? 
-                    <Resume />:
-                    <Link to={url}>{name}</Link>
-                }
-                </h5>
-            </div>
-        </div>
+const sideOptionClassNames = "p-2 sideBtns d-flex justify-content-center"
+
+export default function SideOption({ name, url }) {
+    return name === "Resume" ? (
+        <a href={Pdf} target="_blank" rel="noreferrer" className={sideOptionClassNames}>
+            <h5>Resume</h5>
+        </a>
+    ) : (
+        <Link to={url} className={sideOptionClassNames}>
+            <h5>{name}</h5>
+        </Link>
     )
 }
