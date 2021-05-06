771c428fa2b74ad0dfedae2f66f0da174abf2d5b
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-05 17:35:23 -0700

    changed so homepage is using hooks not class component

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,144 +1,152 @@
-import React, { useState, useEffect } from 'react';
-import './style.css'
-import CoverImg from '../../CoverImg/cover2.jpg'
-import Graph from '../Graph'
-import firstGraph from '../../graphs/first/Skills5_b.png'
-import secondGraph from '../../graphs/second/Skills4_c.png'
-import { Col, Row } from 'react-bootstrap'
-import Bio from '../Bio'
-import ProjectRow from '../ProjectRow'
-import getPillData from '../../services/pillData'
-import Resume from '../Resume'
+//react imports
+import React, { useState, useEffect } from "react"
+import { Col, Row } from "react-bootstrap"
 
-const imgText= [
+//user-defined components
+import Graph from "../Graph"
+import Bio from "../Bio"
+import ProjectRow from "../ProjectRow"
+
+//images
+import firstGraph from "../../graphs/first/Skills5_b.png"
+import secondGraph from "../../graphs/second/Skills4_c.png"
+import coverImg from "../../CoverImg/cover2.jpg"
+
+//services
+import getPillData from "../../services/pillData"
+
+//stylesheet
+import "./style.css"
+
+//global variables
+const graphUrls = [firstGraph, secondGraph]
+
+const timedBlurbText = [
     {
-        first: 'Software',
-        second:'Developer'
+        first: "Software",
+        second: "Developer",
     },
     {
-        first: 'Full-Stack',
-        second:'Developer'
+        first: "Full-Stack",
+        second: "Developer",
     },
     {
-        first: 'Electronics',
-        second:'Nerd'
+        first: "Electronics",
+        second: "Nerd",
     },
     {
-        first: 'Husband',
-        second:''
-    }
+        first: "Husband",
+        second: "",
+    },
 ]
-
-const pillData = {
-    front: "Frontend",
-    back: 'Backend',
-    db: 'Databases',
-    allPills: getPillData('all'),
-    frontendPills: getPillData('frontend'),
-    backendPills: getPillData('backend'),
-    dbPills: getPillData('database')
-}
 
 const timedBlurbOptions = {
     currentImgIndex: 0,
-    imgTextTimeShown: 3.5, //seconds long for testing, prod 3
+    imgTextTimeShown: 3.5, //seconds
 }
 
-const graphUrls = [
-    firstGraph,
-    secondGraph
-]
+const pillData = {
+    front: "Frontend",
+    back: "Backend",
+    db: "Databases",
+    allPills: getPillData("all"),
+    frontendPills: getPillData("frontend"),
+    backendPills: getPillData("backend"),
+    dbPills: getPillData("database"),
+}
 
-const Timer = () => {
-    const [currentImgIdx, setCurrentImgIdx] = useState(0);
-  
-    useEffect(() => {
-      // exit early when we reach 0
-      if (currentImgIdx > 2) setCurrentImgIdx(0);
-  
-      // save intervalId to clear the interval when the
-      // component re-renders
-      const intervalId = setInterval(() => {
-        setCurrentImgIdx(currentImgIdx + 1);
-      }, 1000);
-  
-      // clear interval on re-render to avoid memory leaks
-      return () => clearInterval(intervalId);
+/** components */
 
-      // add timeLeft as a dependency to re-rerun the effect
-      // when we update it
-    }, [currentImgIdx]);
-  
+const HorizontalBreak = () => {
     return (
-        <>
-                <h1>{imgText[currentImgIndex].first}</h1>
-        <br/>
-        <h1>{imgText[currentImgIndex].second}</h1> 
-        </>
-    );
-  };
-
-
-export default function Homepage(props) {
-    
-    return (
-        <>
-            {/*my cover img here */}
-            <figure className='p-0 m-0 border'>
-                <div className='wrapper'>
-                <div className="top-left">
-                    <Timer seconds={5}/>
-                </div>
-                <img className='full-width sendToBack' src={CoverImg} alt=""></img>
-                </div>
-            </figure>
-
-            <div className="ml-5 mr-5">
-                <hr />
-            </div>
-
-            {/* about myself text */}
-            <div className='m-4 '>
-                <Bio />
-            </div>
-
-            <div className="ml-5 mr-5">
-                <hr />
-            </div>
-
-            {/* pills area an heading*/}
-            <div className='m-4'>
-                <h2>
-                    Technologies I like to work with
-                </h2>
-
-                <div className=''>
-                    <ProjectRow rowName={pillData.front} pills={pillData.frontendPills}></ProjectRow>
-                    <ProjectRow rowName={pillData.back} pills={pillData.backendPills}></ProjectRow>
-                    <ProjectRow rowName={pillData.db} pills={pillData.dbPills}></ProjectRow>
-                </div>
-            </div>
-
-            <div className="ml-5 mr-5">
-                <hr />
-            </div>
-
-            {/* skills graphs */}
-            <div className='m-4 '>
-                <h2 className=''>Skills Breakdown</h2>
-                <Row className='full-width '>
-                {
-                    graphUrls.map((graphUrl,i) => {
-                        return (
-                            <Col key={i} xs={12} sm={12} md={12} lg={6}>
-                                <Graph graphUrl={graphUrl}/>
-                            </Col>
-                        )
-                    })
-                }  
-                </Row>
-            </div>
-        </>
+        <div className="ml-5 mr-5">
+            <hr />
+        </div>
     )
 }
 
+const Section = ({ component }) => <div className="m-4 ">{component}</div>
+
+const Tech = () => (
+    <>
+        <h2>Technologies I like to work with</h2>
+
+        <div className="">
+            <ProjectRow rowName={pillData.front} pills={pillData.frontendPills}></ProjectRow>
+            <ProjectRow rowName={pillData.back} pills={pillData.backendPills}></ProjectRow>
+            <ProjectRow rowName={pillData.db} pills={pillData.dbPills}></ProjectRow>
+        </div>
+    </>
+)
+
+const Graphs = () => (
+    <>
+        <h2 className="">Skills Breakdown</h2>
+        <Row className="full-width ">
+            {graphUrls.map((graphUrl, i) => {
+                return (
+                    <Col key={i} xs={12} sm={12} md={12} lg={6}>
+                        <Graph graphUrl={graphUrl} />
+                    </Col>
+                )
+            })}
+        </Row>
+    </>
+)
+
+const TimedBlurb = () => {
+    const [currentImgIdx, setCurrentImgIdx] = useState(timedBlurbOptions.currentImgIndex)
+
+    useEffect(() => {
+        // save intervalId to clear the interval when the
+        // component re-renders
+        const intervalId = setInterval(() => {
+            if (currentImgIdx >= timedBlurbText.length - 1) setCurrentImgIdx(timedBlurbOptions.currentImgIndex)
+            else setCurrentImgIdx(currentImgIdx + 1)
+        }, timedBlurbOptions.imgTextTimeShown * 1000)
+
+        // cleanup - clear interval on re-render to avoid memory leaks
+        //by setting multiple intervals
+        return () => clearInterval(intervalId)
+
+        // add timeLeft as a dependency to re-rerun the effect
+        // when we update it
+    }, [currentImgIdx])
+
+    return (
+        <div className="top-left">
+            <h1>{timedBlurbText[currentImgIdx].first}</h1>
+            <br />
+            <h1>{timedBlurbText[currentImgIdx].second}</h1>
+        </div>
+    )
+}
+
+export default function Homepage(props) {
+    return (
+        <>
+            {/*cover img here */}
+            <figure className="p-0 m-0 border">
+                <div className="wrapper">
+                    <TimedBlurb />
+                    <img className="full-width sendToBack" src={coverImg} alt=""></img>
+                </div>
+            </figure>
+
+            <HorizontalBreak />
+
+            {/* about myself text */}
+            <Section component={<Bio />} />
+
+            <HorizontalBreak />
+
+            {/* pills area an heading*/}
+            <Section component={<Tech />} />
+
+            <HorizontalBreak />
+
+            {/* skills graphs */}
+            <Section component={<Graphs />} />
+        </>
+    )
+}
