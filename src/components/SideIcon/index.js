7854f2c5178d77b262c11d909c83bb7991342a22
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-06 09:35:23 -0700

    cleaned up some warnings

--- src/components/SideIcon/index.js
+++ src/components/SideIcon/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from "react"
 
-function SideIcon({href, src}) {
+function SideIcon({ href, src }) {
     return (
-        <figure className='sideBtns'>
-            <a 
-                target="_blank" 
-                href={href}>
-                <img src={src} />
+        <figure className="sideBtns">
+            <a target="_blank" rel="noreferrer" href={href}>
+                <img src={src} alt="social-icon" />
             </a>
         </figure>
     )
--- src/services/projectData.js
+++ src/services/projectData.js
@@ -1,34 +1,34 @@
-import  getPillData  from "./pillData";
-import stacks from './techStacks'
+import getPillData from "./pillData"
 
-
-const projects =  [
+const projects = [
     {
-        name: 'Online Whiteboard',
-        img: '', //or gif
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        urlBlurb: 'Play online now!',
-        url: '', //where to find project or source code
-        pills: getPillData(['React', 'Node.js', 'Express'])
+        name: "Online Whiteboard",
+        img: "", //or gif
+        desc:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        urlBlurb: "Play online now!",
+        url: "", //where to find project or source code
+        pills: getPillData(["React", "Node.js", "Express"]),
     },
     {
-        name: 'Country Comparison Tool',
-        img: '', //or gif
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        urlBlurb: 'Use it now!',
-        url: 'https://rboesp.github.io/Interactive-Visualization/',
-        pills: getPillData(['Chart.js', 'Node.js', 'Express'])
+        name: "Country Comparison Tool",
+        img: "", //or gif
+        desc:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        urlBlurb: "Use it now!",
+        url: "https://rboesp.github.io/Interactive-Visualization/",
+        pills: getPillData(["Chart.js", "Node.js", "Express"]),
     },
     {
-        name: 'Network Pong',
-        img: '', //or gif
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        urlBlurb: 'Use it now!',
-        url: 'https://networking-pong.herokuapp.com/',
-        pills: getPillData(['Node.js', 'Express'])
+        name: "Network Pong",
+        img: "", //or gif
+        desc:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        urlBlurb: "Use it now!",
+        url: "https://networking-pong.herokuapp.com/",
+        pills: getPillData(["Node.js", "Express"]),
     },
 ]
-
 
 export default function getProjects() {
     return projects
