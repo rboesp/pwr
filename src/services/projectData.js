96c25adb4ee4f0ba815d09fc97c7aae7d42cdf95
Robert Boespflug <boespflr@oregonstate.edu>
2021-05-20 13:34:02 -0700

    fixed small error

--- src/services/projectData.js
+++ src/services/projectData.js
@@ -21,7 +21,7 @@ const projects = [
         name: "Online Pong 🏓",
         desc: "Play pong with people over the internet!",
         urlBlurb: "Play now!",
-        url: "https://rboesp.github.io/Networking-Pong/,
+        url: "https://rboesp.github.io/Networking-Pong/",
         pills: getPillData(["Node.js", "Express"]),
         src: `https://i.ibb.co/KVX9Rhw/pong-gif2.gif`,
     },
